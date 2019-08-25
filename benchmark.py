130ff8aadb15bebf092bc7d82f6dfe95a861792b
Jack Koenig <koenig@sifive.com>
2019-08-24 19:17:37 -0700

    Add support for benchmarking across JVMs

--- benchmark.py
+++ benchmark.py
@@ -48,8 +48,9 @@ def extract_run_time(output):
     else :
         raise Exception('Runtime not found!')
 
-def run_firrtl(jar, design):
-    cmd = time() + ['java', '-cp', jar, 'firrtl.Driver', '-i', design,'-o','out.v','-X','verilog']
+def run_firrtl(java, jar, design):
+    java_cmd = java.split()
+    cmd = time() + java_cmd + ['-cp', jar, 'firrtl.stage.FirrtlMain', '-i', design,'-o','out.v','-X','verilog']
     result = subprocess.run(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     if result.returncode != 0 :
         print(result.stdout)
@@ -57,7 +58,6 @@ def run_firrtl(jar, design):
         sys.exit(1)
     size = extract_max_size(result.stderr.decode('utf-8'))
     runtime = extract_run_time(result.stdout.decode('utf-8'))
-    print('{} B, {} ms'.format(size, runtime))
     return (size, runtime)
 
 def parseargs():
@@ -68,6 +68,8 @@ def parseargs():
                         help='FIRRTL commit hashes to benchmark')
     parser.add_argument('--iterations', '-N', type=int, default=10,
                         help='Number of times to run each benchmark')
+    parser.add_argument('--jvms', type=str, nargs='+', default=['java'],
+                        help='JVMs to use')
     return parser.parse_args()
 
 def get_version_hashes(versions):
@@ -131,17 +133,22 @@ def main():
     check_designs(designs)
     hashes = get_version_hashes(args.versions)
     jars = build_firrtl_jars(hashes)
+    jvms = args.jvms
     N = args.iterations
-    info = [['revision', 'design', 'max heap', 'SD', 'runtime', 'SD']]
-    for hashcode, jar in jars.items():
-        print("Benchmarking {}...".format(hashcode))
-        revision = hashcode
-        for design in designs:
-            print('Running {}...'.format(design))
-            (sizes, runtimes) = zip(*[run_firrtl(jar, design) for i in range(N)])
-            norm_sizes = norm_max_set_sizes(sizes)
-            info.append([revision, design, median(norm_sizes), stdev(norm_sizes), median(runtimes), stdev(runtimes)])
-            revision = ''
+    info = [['java', 'revision', 'design', 'max heap', 'SD', 'runtime', 'SD']]
+    for java in jvms:
+        print("Running with '{}'".format(java))
+        for hashcode, jar in jars.items():
+            print("Benchmarking {}...".format(hashcode))
+            revision = hashcode
+            java_title = java
+            for design in designs:
+                print('Running {}...'.format(design))
+                (sizes, runtimes) = zip(*[run_firrtl(java, jar, design) for i in range(N)])
+                norm_sizes = norm_max_set_sizes(sizes)
+                info.append([java_title, revision, design, median(norm_sizes), stdev(norm_sizes), median(runtimes), stdev(runtimes)])
+                java_title = ''
+                revision = ''
 
     for line in info:
         formated = ['{:0.2f}'.format(elt) if isinstance(elt, numbers.Real) else elt for elt in line]
